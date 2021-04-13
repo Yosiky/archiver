eaf200cec404402f49dc5d3ddb70684f2dbcb18d
yaskovMC <user1@armmiet01.ete.loc>
2021-04-13 15:59:56 +0300

    Edit file_work.c

--- file_work.c
+++ file_work.c
@@ -11,13 +11,28 @@ FILE* file_open()
 
 size_t* file_read(FILE* file)
 {
+	const size_t SIZE = 256;
 	size_t* mas = (size_t*)calloc(MAX_SIZE_STR, sizeof(size_t));
-	int value;
+	char buff[256];
+	char flag = 1;
+	while (flag) {
+		fgets(str, SIZE, file);
+		for (size_t i = 0; i < SIZE && flag; ++i) {
+			if (buff[i] == EOF) {
+				flag = 0;
+				break;
+			}
+			mas[buff[i]]++;
+		}
+		mas[value]++;
+	}
+/*
 	do {
 		value = fgetc(file);
 		if (value == EOF)
 			break;
 		mas[value]++;
 	} while (value != EOF);
+*/
 	return mas;
 }
